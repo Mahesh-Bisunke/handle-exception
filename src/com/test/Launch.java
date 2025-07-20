9d43eeb7e059051fc7e5477abefe8e5083664b64
Mahesh-Bisunke <maheshbisunke92@gmail.com>
2025-07-20 22:51:03 +0545

    Exception Example in java

---
+++ src/com/test/Launch.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number1:");
+		int num1 = sc.nextInt();
+		System.out.println("Enter number2:");
+		int num2 = sc.nextInt();
+		System.out.println(num1+num2);
+	}
+}
