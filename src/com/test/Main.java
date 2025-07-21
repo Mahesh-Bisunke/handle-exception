bc8faa4c57c4199df467a18678d728a1fe1dd89a
Mahesh-Bisunke <maheshbisunke92@gmail.com>
2025-07-21 12:17:05 +0545

    Main.java added for Exeption Handling

---
+++ src/com/test/Main.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+	ArithmeticExceptionExample ae = new ArithmeticExceptionExample();
+
+	int ck = ae.checkException();
+	System.out.println(ck);
+	}
+
+}
